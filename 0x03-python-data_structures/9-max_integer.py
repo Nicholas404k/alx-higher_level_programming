df479350cff156ef94739202b33e82f3249e62ce
Nicholas404k <ballloner8@gmail.com>
2023-09-11 21:07:56 +0000

    9-max_integer.py 6-print_matrix_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        maxx = my_list[0]
-        for i in my_list:
-            if i > maxx:
-                maxx = i
-                return maxx
+    if my_list == []:
+        return None
+    tmp = my_list[0]
+    for i in my_list:
+        if i > tmp:
+            tmp = i
+            return tmp
