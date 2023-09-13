f7cb30055f835bcf519de260e66c70013a1deec2
Nicholas404k <ballloner8@gmail.com>
2023-09-13 12:42:05 -0700

    8-simple_delete.py 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-        if a_dictionary is None:
-                return None
-        best = 0
-        best_key = None
-        for key, val in a_dictionary.items():
-                if val > best:
-                        best = val
-                        best_key = key
-        return best_key
+    win_n = 0
+    winner = None
+    if type(a_dictionary) is dict:
+        for (key, value) in a_dictionary.items():
+            if value > win_n:
+                win_n = value
+                winner = key
+    return winner
